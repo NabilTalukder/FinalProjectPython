1c8f41a9f8614c6f12ae7137a275b62c60f158b1
NabilTalukder <71780298+NabilTalukder@users.noreply.github.com>
2024-04-13 21:42:23 +0100

    if statements continues instead of breaking

--- AppData/Local/Programs/Python/quizPrompter.py
+++ AppData/Local/Programs/Python/quizPrompter.py
@@ -8,10 +8,10 @@ client = OpenAI(api_key = OPENAI_API_KEY)
 
 def generateQuiz(qgi):
 
-  prompt = '''Create a multiple choice quiz with 10 questions.
+  prompt = '''Create a multiple choice quiz with 3 questions.
   The first line should say exactly: Multiple Choice Quiz.
   Every question should be listed like so: #X: Q where X is replaced by the question number and Q is the question.
-  Each option should be listed like so: Y) where Y is a capital letter, in alphabetical order to distinguish the options.
+  There should be 4 options, where each option should be listed like so: Y) where Y is a capital letter, in alphabetical order to distinguish the options.
   The answer to each question should be written under the last option of each question like so: Answer: Z) A 
   where Z is the letter corresponding to the answer and A is the answer text, matching exactly the text in the corresponding option.
   Create the quiz based on the information in the following text:''' +  qgi
@@ -40,10 +40,11 @@ while True:
     clientSocket, address = serverSocket.accept()
     print("Server connected client ", address)
     
-    #recieve input from Java client and break once end reached
+    #receive input from Java client 
+    print("awaiting request")
     data = clientSocket.recv(1024)
     if not data:
-       break
+       continue
     print("received quizGenInput")
     #generate quix from user inputted text and send output back to client
     quizGenOutput = generateQuiz(data.decode())
